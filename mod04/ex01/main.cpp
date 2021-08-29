5a457b230f3c09ac977925b46517c20e48901614
pmarash <stepakirillov59@gmail.com>
2021-08-29 20:34:38 +0300

    modyfied ex01

--- mod04/ex01/main.cpp
+++ mod04/ex01/main.cpp
@@ -5,9 +5,12 @@
 int		main(void)
 {
 	const Animal	*cat = new Cat();
+	std::cout << std::endl;
 	const Animal	*dog = new Dog();
+	std::cout << std::endl;
 
 	delete cat;
+	std::cout << std::endl;
 	delete dog;
 	return (0);
 }
